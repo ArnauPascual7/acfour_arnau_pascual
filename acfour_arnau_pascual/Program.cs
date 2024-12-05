538e9745546b5ffdf0aa872ab350d010bcada8e4
Arnau Pascual <arnau.pascual.7e8@itb.cat>
2024-12-05 09:53:30 +0100

    Add project files.

---
+++ acfour_arnau_pascual/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace acfour_arnau_pascual
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
