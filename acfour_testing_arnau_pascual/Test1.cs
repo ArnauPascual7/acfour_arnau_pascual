c42fe2bbab08630de75c8f51981a9bf2a1d7d280
Arnau Pascual <arnau.pascual.7e8@itb.cat>
2024-12-15 12:01:16 +0100

    Unit Testting OrderNumbers() unit testing

---
+++ acfour_testing_arnau_pascual/Test1.cs
@@ -0,0 +1,20 @@
+﻿namespace acfour_testing_arnau_pascual
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestOrderNumbers()
+        {
+            // Arrange
+            int[] arr = new int[] { 1005, 0, 43, 89, 245, 1, 99, 7, 999, 576, 2 , 1006 };
+            int[] expArr = new int[] { 1006, 1005, 999, 576, 245, 99, 89, 43, 7, 2, 1, 0 };
+
+            // Act
+            acfour_arnau_pascual.Program.OrderNumbers(arr);
+
+            // Assert
+            CollectionAssert.AreEqual(expArr, arr);
+        }
+    }
+}
